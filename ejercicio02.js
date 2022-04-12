6b3474656094e1ab1e409ac739f148b7ac61b250
laopengyou <stf30111994@gmail.com>
2022-04-12 22:28:22 +0200

    thebridge JS02

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,49 +1,3 @@
-/** 
-let diaActual=new Date();
-const dia_de_la_semana=diaActual.getDay();
-switch(dia_de_la_semana){
-    case 1:
-        console.log("Es lunes");
-        break;
-    case 2:
-        console.log("Es martes");
-        break;
-    case 3:
-        console.log("Es miercoles");
-    //....
-}
-*/
-//case?
-//%
-//**
-
-/*const vueltas = [1,2,3,4,5,6,7,8,9,10];
-
- for (let vuelta of vueltas) {
-        console.log("Vuelta nº " + vuelta);
- }*/
-
-/*const person = { fname: "John", lname: "Doe", age: 25 };
-     
- for (let x in person) {
-        console.log(x)
- }*/
-
- //function
- function sum(a,b){
-     return a-b
- }
-
-console.log(sum(2,3));
-
-/*function greet(greeting="hello",name){
-    if(!name){
-        return greeting;
-    }else{
-        return'$()greeting..'
-    }
-}*/
-
 //1.Booleanos
 
 let booleano1=true;
@@ -52,13 +6,13 @@ let booleano2=false;
 const booleanoAnd=booleano1 && booleano2;
 console.log(booleanoAnd);
 
-const booleanoOr=booleano1 || booleano2;
+const booleanoOr=!booleano1;
 console.log(booleanoOr);
 
-const booleanoNot=booleano1 !== booleano2;
+const booleanoNot=booleano1;
 console.log(booleanoNot);
 
-//const booleanoMix0=(booleano1||booleano2)&&();
+const booleanoMix0=booleano1||booleano2&&(booleano1||(!booleano1&&!booleano2));
 
 //2.Operadores
 
@@ -77,25 +31,20 @@ console.log(typeof B);
 console.log(A==B);
 console.log(A===B);
 
-
 const mochila=[1,2,3,4,5,6,7,8,9,10];
-console.log("Objetos en la mochila: ", mochila.length)
-        if(mochila.length > 10){
-            console.log("No puedo cargar con tantas cosas");
-        }else if(mochila.length <= 10 && mochila.length > 2){
-            console.log("Que bien voy con mi mochila");
-        }else{
-            console.log("Creo q no necesito una mochila");
-        }
+    if(mochila.length>10){
+        console.log("No puedo cargar con tantas cosas");
+    }else if(mochila.length<10 && mochila.length>2){
+        console.log("Que bien voy con mi mochila");
+    }else{
+        console.log("Creo q no necesito una mochila");
+    }
 
-//---------------------------------------------------
-const numero=10;
-        let contarHsta10=0;
-        while(contarHsta10<=numero){
-            console.log(contarHsta10);
-            contarHsta10++;
-        }
-//---------------------------------------------------
+
+for(let contarHasta10=0 ; contarHasta10 < 11 ; contarHasta10++){
+    console.log(contarHasta10);
+}
+    
 
 const diaFestivo=true;
         if(diaFestivo){
@@ -104,20 +53,81 @@ const diaFestivo=true;
             console.log("Hoy trabajo");
         }
 
-//------------------------------------------
+
 const arrayBucle=[];
-        for(let i = 4; i < 19; i++){
-            arrayBucle.push(i)
+        for(let i=3 ; i < 19 ; i++){
+            arrayBucle.push(i);
         }
-    console.log(arrayBucle);
-//-----------------------------------------
+        console.log(arrayBucle);
 
-//--------------------------------
-//---------------------------------
+/*
+const Dotaheros=['necro', 'ursa', 'od', 'pl']
+        let zhenrong=" ";
+            for (let dotahero of Dotaheros){
+                text += dotahero +" ";
+            }
+            console.log(text);*/
 
-const arrayUno=['Con', 'Sofia', 'aprendiendo', 'bucles']
+const arrayUno=['Con', 'Sofia', 'aprendiendo', 'bucles'];
+    let text=" ";
+    for (let x in arrayUno){
+        text += x + " ";
+    }
+        console.log(text);
+        console.log(arrayUno[2]);
 
 
-        
-        
-        
+console.log("loop 20times")
+let i =0;
+while(i<20){
+    if(i%3==0)
+    {
+        console.log(i);
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+
+let notaActual =[1,2,3,4,5,6,7,8,9,10];
+    const nota = notaActual[7];
+    switch (nota) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            console.log("Insuficiente");
+            break;
+        case 5:
+        case 6:
+            console.log("Suficiente");
+            break;
+        case 7:
+        case 8:
+            console.log("Notable");
+            break;
+        case 9:
+        case 10:
+            console.log("Sobresaliente"); 
+            break;    
+    }
+
+
+function resta(a,b){
+    return a-b
+}
+console.log(resta(3,2));
+
+
+function duplicaNumero(x){
+    if(x!=null){
+        return x*2
+    }
+    else {
+        return "Debo ser ejecutada con un número"
+    }
+}
+console.log(duplicaNumero())
